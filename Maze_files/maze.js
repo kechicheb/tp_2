270d3bdb6284181671a964e5011625536072a3ee
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-10-14 17:18:38 +0200

    added Exo 4

--- Maze_files/maze.js
+++ Maze_files/maze.js
@@ -1,28 +1,56 @@
-// Exo 01
-// window.onload = () => {
-//   document.querySelectorAll("#maze .boundary").forEach((boundary) => {
-//     boundary.onmouseover = () => {
-//       boundary.style.backgroundColor = "red";
-//     };
-//   });
-// };
-
-//Exo 02
-
-// window.onload = () => {
-//     let allBoundary  = Array.from(document.querySelectorAll("#maze .boundary"));
-//    for(let i =0;i<allBoundary.length;i++) {
-//     allBoundary[i].addEventListener("mouseover",()=> {
-//         for(let j  =0;j<allBoundary.length;j++) {
-//             allBoundary[j].style.backgroundColor = "red"
-//         }
-//     })
-//    }
-// };
-
-//  Exo 03
 window.onload = () => {
-  document.getElementById("end").onmouseover = () => {
-    alert("You win!");
+  const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if (element) return element;
+    throw new Error(`Cannot find the element ${selector} `);
   };
+  let status = true;
+  // Exo 01
+
+  //   document.querySelectorAll("#maze .boundary").forEach((boundary) => {
+  //     boundary.onmouseover = () => {
+  //       boundary.style.backgroundColor = "red";
+  //     };
+  //   });
+
+  //Exo 02 ***************************
+
+  lost();
+
+  //  Exo 03 *************************************
+
+  selectElement("#end").onmouseover = () => {
+    if (status == true) {
+      alert("You Win !");
+    } else {
+      alert("You Lost !");
+    }
+  };
+
+  //***************************************** */
+  // Exo 04
+
+  status = true;
+  let allBoundary = Array.from(document.querySelectorAll("#maze .boundary"));
+  let start = selectElement("#start");
+  start.addEventListener("click", () => {
+    allBoundary.forEach((e) => {
+      e.style.backgroundColor = "#eeeeee";
+    });
+    status = true;
+  });
+
+  //   ********************************
+
+  function lost() {
+    let allBoundary = Array.from(document.querySelectorAll("#maze .boundary"));
+    for (let i = 0; i < allBoundary.length; i++) {
+      allBoundary[i].addEventListener("mouseover", () => {
+        for (let j = 0; j < allBoundary.length; j++) {
+          allBoundary[j].style.backgroundColor = "red";
+        }
+        status = false;
+      });
+    }
+  }
 };
